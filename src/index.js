4d33127d18ec735ba18bf35166cc742ac0c83c3b
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2024-08-18 13:43:02 +0530

    Updated DB Imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 // require("dotenv").config({path:'./.env'});
-import dotenv from 'dotenv/config';
+// import dotenv from 'dotenv/config';
+import connectDB from './db/index.js';
+
+import dotenv from 'dotenv';
 
 dotenv.config(
     { path: './.env' }
 );
+
+connectDB()
 
 /*
 (async () => {
